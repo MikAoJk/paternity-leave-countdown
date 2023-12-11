436e56296c183759a3144eb7c39b8a589003d29f
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-12-11 19:34:48 +0100

    Added in BarnevognIkon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import {Countdown} from "@/components/Countdown";
+import React from "react";
+import BarnevognIkon from "@/components/BarnevognIkon";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
+      <main>
+          <div className="flex flex-col items-center justify-between p-10">
+            <BarnevognIkon/>
+      </div>
+    <div className="flex min-h-screen flex-col items-center justify-between md:p-12">
       <div className="z-5 items-center justify-between font-mono text-sm flex">
           <Countdown date={`2023-12-21T00:00:00`} />
       </div>
+    </div>
     </main>
   )
 }
